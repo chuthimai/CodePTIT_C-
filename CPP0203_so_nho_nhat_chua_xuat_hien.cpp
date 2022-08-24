dfd46c38d7d84ac9bb5e3747edc6c384ceaa123b
Chu Mai <chuthimai18245@gmail.com>
2022-08-24 22:18:43 +0700

    use vector to sort and delete the number have same value use a variable to mark

---
+++ CPP0203_so_nho_nhat_chua_xuat_hien.cpp
@@ -0,0 +1,40 @@
+//
+//  CPP0203_so_nho_nhat_chua_xuat_hien.cpp
+//  CodePTIT_C++
+//
+//  Created by Chu Mai on 24/08/2022.
+//
+
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+void oneTime(){
+    vector<ll> arr;
+    int n; cin>>n;
+    for(int i=0;i<n;i++){
+        ll a;
+        cin>>a;
+        arr.push_back(a);
+    }
+    sort(arr.begin(), arr.end());
+    arr.erase(unique(arr.begin(), arr.end()), arr.end());
+    
+    unsigned int m=1;
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]<m) continue;
+        else if(arr[i]>m) {
+            printf("%u\n",m);
+            break;
+        }
+        else if(arr[i]==m){
+            m++;
+            if(i==arr.size()-1) printf("%u\n",m);
+        }
+    }
+}
+
+int main(){
+    int n; cin>>n;
+    for(int i=0;i<n;i++) oneTime();
+}
--- Ex1.cpp
+++ Ex1.cpp
@@ -9,19 +9,31 @@
 //
 //using namespace std;
 //
+//struct student{
+//    string MSV;
+//    string name;
+//    string lop;
+//    float dtbcpp;
+//    float dtbt2;
+//};
+//
 //int main(){
-//    string s;
-//    getline(cin, s);
-//    int trans='A'-'a';
+//    int n;
+//    cin>>n;
+//    student arr[100];
+//    for(int i=0;i<n;i++){
+//        cin>>arr[i].MSV;
+//        cin.ignore();
+//        getline(cin, arr[i].name);
+//        cin>>arr[i].lop;
+//        cin>>arr[i].dtbt2;
+//        cin>>arr[i].dtbcpp;
+//    }
 //    
-//    for(int i=0;i<s.length();i++){
-//        if(s[i]>='A'&& s[i]<='Z'){
-//            s[i]=s[i]-trans;
-//        }
+//    for(int i=0;i<n;i++){
+//        if(arr[i].dtbcpp>=7) cout<<"\n"<<arr[i].MSV<<"\n"<<arr[i].name<<"\n"<<arr[i].lop<<"\n"<<arr[i].dtbt2<<"\n"<<arr[i].dtbcpp<<"\n";
 //    }
-//    string s1="";
-//    int m=s.length()-1;
-//    while (s[m]!=' ') {
-//        s1.;
-//    }
+//    
+//    
 //}
+//

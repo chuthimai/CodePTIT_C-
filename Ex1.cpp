35313b1825c8fe6454a5ca8de5997df34b076556
Chu Mai <chuthimai18245@gmail.com>
2022-09-05 22:12:56 +0700

    Update

--- Ex1.cpp
+++ Ex1.cpp
@@ -5,35 +5,14 @@
 //  Created by Chu Mai on 16/08/2022.
 //
 
-//#include <bits/stdc++.h>
-//
-//using namespace std;
-//
-//struct student{
-//    string MSV;
-//    string name;
-//    string lop;
-//    float dtbcpp;
-//    float dtbt2;
-//};
-//
-//int main(){
-//    int n;
-//    cin>>n;
-//    student arr[100];
-//    for(int i=0;i<n;i++){
-//        cin>>arr[i].MSV;
-//        cin.ignore();
-//        getline(cin, arr[i].name);
-//        cin>>arr[i].lop;
-//        cin>>arr[i].dtbt2;
-//        cin>>arr[i].dtbcpp;
-//    }
-//    
-//    for(int i=0;i<n;i++){
-//        if(arr[i].dtbcpp>=7) cout<<"\n"<<arr[i].MSV<<"\n"<<arr[i].name<<"\n"<<arr[i].lop<<"\n"<<arr[i].dtbt2<<"\n"<<arr[i].dtbcpp<<"\n";
-//    }
-//    
-//    
-//}
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ifstream input("Hello.txt");
+    string num;
+    getline(input,num);
+    cout<<num<<endl;
+    input.close();
+}

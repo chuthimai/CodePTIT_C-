7defea13cf1c660253a7375293785d30c67e4c59
Chu Mai <chuthimai18245@gmail.com>
2022-09-11 22:47:27 +0700

    add CPP0801_sao_chep_tep_tin.cpp

---
+++ CPP0801_sao_chep_tep_tin.cpp
@@ -0,0 +1,21 @@
+//
+//  CPP0801_sao_chep_tep_tin.cpp
+//  CodePTIT_C++
+//
+//  Created by Chu Mai on 10/09/2022.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ifstream in{"PTIT.in"};
+    ofstream out{"PTIT.out"};
+    out.clear();
+    string str;
+    while (in>>str) {
+        out<<str<<endl;
+    }
+    in.close();
+    out.close();
+}
